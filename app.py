d6a234aff1a42a8dbc5a5e4a8d5f38913976670b
hy2021-IT <84999990+hy2021-IT@users.noreply.github.com>
2021-12-10 21:21:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,27 @@ import requests
 
 app = Flask(__name__)
 
-API_URL = "https://apodapi.herokuapp.com/api/" # 1. Insert API request URL
+API_URL = "https://api.eva.pingutil.com/email?email=test@mail7.io" # 1. Insert API request URL
 
 @app.route('/')
 def index():
     # 2. Some APIs need a payload or extra data in the fields, set them up here:
-    payload = {}
+    payload = {"email_address": "test@mail7.io",
+        "domain": "mail7.io",
+        "valid_syntax": true,
+        "disposable": true,
+        "webmail": false,
+        "deliverable": true,
+        "catch_all": true,
+        "gibberish": false,
+        "spam": false}
     
     # 3. Get the API Response in JSON
-    api_response = requests.get(API_URL, params=payload).json()
+    api_response = requests.get(https://api.eva.pingutil.com/email?email=test@mail7.io, params=payload).json()
     
     # 4. Extract the information for the template
-    title = api_response["title"]
-    image_url = api_response["url"]
+    title = api_response["Verify the email instantly"]
+    image_url = api_response["https://api.eva.pingutil.com/email?email=test@mail7.io"]
 
     # 5. Return the Template with your data
     return render_template("index.html", title=title, image_url=image_url)
